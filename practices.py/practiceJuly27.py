28eb6696fc2466ff6722e9d11f0e1aee3ce7a88c
Afra Dur <afra.dur@gmail.com>
2020-08-14 20:41:57 -0400

    homework

---
+++ practices.py/practiceJuly27.py
@@ -0,0 +1,15 @@
+print('hello world')
+# To print a string we close it in quotes
+print('July is almost over')
+print("Today's a hot day!")
+print('the instructor: "made it clear" and left.')
+
+
+print('''...lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tincidunt hendrerit. Nulla sed suscipit justo. Maecenas convallis arcu eu volutpat interdum. Nulla nec dolor risus. Phasellus at nisl tempor, ullamcorper nisi eu, aliquam orci. Aliquam at nisi ut felis dapibus mattis ultrices at velit. Cras dui nisi, mollis euismod diam in, laoreet gravida leo. Cras aliquam imperdiet purus ut varius. Nullam tristique urna eget posuere blandit. Aliquam fringilla efficitur nisl in auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus mollis metus malesuada convallis aliquet.''')
+# python data types: things on the keyboard: str, int, list
+x = "var_pink"
+y = 2.5
+z = ["clean", "cook", "study"]
+print(x) #string
+print(y) #float
+print(z) #list
